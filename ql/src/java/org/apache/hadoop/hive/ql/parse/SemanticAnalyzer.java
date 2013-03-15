af4703c2580f0dcb5e0ce5d843e90d7fdc1dafaa
navis <navis.ryu@nexr.com>
2013-03-15 14:56:45 +0900

    HIVE-4181 [jira] Star argument without table alias for UDTF is not working
    Summary:
    HIVE-4181 Star argument without table alias for UDTF is not working
    
    select explode as x from src; thorws
    
    java.lang.NullPointerException
    	at org.apache.hadoop.hive.ql.parse.SemanticAnalyzer.genUDTFPlan(SemanticAnalyzer.java:5344)
    	at org.apache.hadoop.hive.ql.parse.SemanticAnalyzer.genSelectPlan(SemanticAnalyzer.java:2526)
    	at org.apache.hadoop.hive.ql.parse.SemanticAnalyzer.genSelectPlan(SemanticAnalyzer.java:2284)
    	at org.apache.hadoop.hive.ql.parse.SemanticAnalyzer.genPostGroupByBodyPlan(SemanticAnalyzer.java:7079)
    	at org.apache.hadoop.hive.ql.parse.SemanticAnalyzer.genBodyPlan(SemanticAnalyzer.java:7050)
    	at org.apache.hadoop.hive.ql.parse.SemanticAnalyzer.genPlan(SemanticAnalyzer.java:7792)
    	at org.apache.hadoop.hive.ql.parse.SemanticAnalyzer.analyzeInternal(SemanticAnalyzer.java:8462)
    	at org.apache.hadoop.hive.ql.parse.BaseSemanticAnalyzer.analyze(BaseSemanticAnalyzer.java:259)
    	at org.apache.hadoop.hive.ql.Driver.compile(Driver.java:433)
    	at org.apache.hadoop.hive.ql.Driver.compile(Driver.java:337)
    	at org.apache.hadoop.hive.ql.Driver.run(Driver.java:902)
    	at org.apache.hadoop.hive.cli.CliDriver.processLocalCmd(CliDriver.java:259)
    	at org.apache.hadoop.hive.cli.CliDriver.processCmd(CliDriver.java:216)
    	at org.apache.hadoop.hive.cli.CliDriver.processLine(CliDriver.java:413)
    	at org.apache.hadoop.hive.cli.CliDriver.run(CliDriver.java:756)
    	at org.apache.hadoop.hive.cli.CliDriver.main(CliDriver.java:614)
    	at sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)
    	at sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:39)
    	at sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:25)
    	at java.lang.reflect.Method.invoke(Method.java:597)
    	at org.apache.hadoop.util.RunJar.main(RunJar.java:186)
    
    Test Plan: EMPTY
    
    Reviewers: JIRA
    
    Differential Revision: https://reviews.facebook.net/D9453

--- ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzer.java
+++ ql/src/java/org/apache/hadoop/hive/ql/parse/SemanticAnalyzer.java
@@ -2472,6 +2472,7 @@ public class SemanticAnalyzer extends BaseSemanticAnalyzer {
       posn++;
     }
 
+    boolean subQuery = qb.getParseInfo().getIsSubQ();
     boolean isInTransform = (selExprList.getChild(posn).getChild(0).getType() ==
         HiveParser.TOK_TRANSFORM);
     if (isInTransform) {
@@ -2506,6 +2507,10 @@ public class SemanticAnalyzer extends BaseSemanticAnalyzer {
       if (isUDTF && !fi.isNative()) {
         unparseTranslator.addIdentifierTranslation((ASTNode) udtfExpr
             .getChild(0));
+      }
+      if (isUDTF && (selectStar = udtfExprType == HiveParser.TOK_FUNCTIONSTAR)) {
+        genColListRegex(".*", null, (ASTNode) udtfExpr.getChild(0),
+            col_list, inputRR, pos, out_rwsch, qb.getAliases(), subQuery);
       }
     }
 
@@ -2607,7 +2612,6 @@ public class SemanticAnalyzer extends BaseSemanticAnalyzer {
 
       }
 
-      boolean subQuery = qb.getParseInfo().getIsSubQ();
       if (expr.getType() == HiveParser.TOK_ALLCOLREF) {
         pos = genColListRegex(".*", expr.getChildCount() == 0 ? null
             : getUnescapedName((ASTNode) expr.getChild(0)).toLowerCase(),
