3265af5c3ac340ae41271151f26f2db0f7329f96
navis <navis.ryu@nexr.com>
2013-05-28 16:54:58 +0900

    HIVE-4619 Hive 0.11.0 is not working with cdh3u5

--- ql/src/java/org/apache/hadoop/hive/ql/exec/MapOperator.java
+++ ql/src/java/org/apache/hadoop/hive/ql/exec/MapOperator.java
@@ -470,6 +470,8 @@ public class MapOperator extends Operator<MapredWork> implements Serializable, C
     Path fpath = new Path(HiveConf.getVar(hconf,
         HiveConf.ConfVars.HADOOPMAPFILENAME));
 
+    boolean schemeless = fpath.toUri().getScheme() == null;
+
     ArrayList<Operator<? extends OperatorDesc>> children =
       new ArrayList<Operator<? extends OperatorDesc>>();
     opCtxMap = new HashMap<MapInputPath, MapOpCtx>();
@@ -481,6 +483,9 @@ public class MapOperator extends Operator<MapredWork> implements Serializable, C
       for (String onefile : conf.getPathToAliases().keySet()) {
         MapOpCtx opCtx = initObjectInspector(conf, hconf, onefile, convertedOI);
         Path onepath = new Path(onefile);
+        if (schemeless) {
+          onepath = new Path(onepath.toUri().getPath());
+        }
         List<String> aliases = conf.getPathToAliases().get(onefile);
 
         for (String onealias : aliases) {
@@ -747,4 +752,9 @@ public class MapOperator extends Operator<MapredWork> implements Serializable, C
     return null;
   }
 
+  public static void main(String[] args) {
+    Path path = new Path("hdfs://qa14:9000/user/hive/warehouse/data_type");
+    Path path2 = new Path("/user/hive/warehouse/data_type/000000_0");
+    System.err.println("[MapOperator/main] " + path.toUri().relativize(path2.toUri()));
+  }
 }
