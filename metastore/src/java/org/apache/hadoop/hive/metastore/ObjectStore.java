58ccb2b5c32e9291e3bf85987f0384570f489b50
navis <navis@nexr.com>
2014-03-24 09:41:48 +0900

    HIVE-4767 ObjectStore.getPMF has concurrency problems

--- metastore/src/java/org/apache/hadoop/hive/metastore/ObjectStore.java
+++ metastore/src/java/org/apache/hadoop/hive/metastore/ObjectStore.java
@@ -261,7 +261,7 @@ public class ObjectStore implements RawStore, Configurable {
     return prop;
   }
 
-  private static PersistenceManagerFactory getPMF() {
+  private static synchronized PersistenceManagerFactory getPMF() {
     if (pmf == null) {
       pmf = JDOHelper.getPersistenceManagerFactory(prop);
       DataStoreCache dsc = pmf.getDataStoreCache();
